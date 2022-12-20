2a419dc699552ae8451c709175c4330c0fda1fb2
Stanislav Giley <stanislavgiley@MacBook-Pro-Stanislav-2.local>
2022-12-20 12:55:15 +0300

    add sharing option

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mynotes/services/cloud/cloud_note.dart';
-import 'package:mynotes/services/crud/note_service.dart';
 import 'package:mynotes/utilities/dialogs/delete_dialog.dart';
 
 typedef NoteCallback = void Function(CloudNote note);
