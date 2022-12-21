9fdf73ed47f738d2a74f9c54a2063899d7443e09
Stanislav Giley <stanislavgiley@MacBook-Pro-Stanislav-2.local>
2022-12-21 17:36:39 +0300

    fix security problem

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ void main() {
   runApp(
     MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
